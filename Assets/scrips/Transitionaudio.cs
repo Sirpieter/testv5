c16560a0da2611a5a3be471e29dd390cd4159426
Pieter <pieter.dieleman@gmail.com>
2018-06-28 22:34:55 +0200

    aanpassingen

---
+++ Assets/scrips/Transitionaudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Transitionaudio : MonoBehaviour {
+
+    public AudioMixer mixer;
+    private AudioMixerSnapshot normalSnapshot;
+    private AudioMixerSnapshot lowVolumeSnapshot;
+
+	// Use this for initialization
+	void Start () 
+    {
+        normalSnapshot = mixer.FindSnapshot("Normal");
+        lowVolumeSnapshot = mixer.FindSnapshot("LowVolume");
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            mixer.ClearFloat("MasterVolume");
+            lowVolumeSnapshot.TransitionTo(0.5f);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            float currentVolume;
+            mixer.GetFloat("MasterVolume", out currentVolume);
+
+            mixer.SetFloat("MasterVolume", currentVolume + .1f);
+        }
+	}
+}
